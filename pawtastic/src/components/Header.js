cd2338f6b1e8ccc944595232a07eca3e020eff38
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-14 14:39:46 -0500

    working

---
+++ pawtastic/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Header extends React.Component{
+    render(){
+    return(
+        <div className= "title">
+            <h2>Yay, we love dogs! Give us the basics about your pup.</h2>
+        </div>
+    );
+    }
+}
+export default Header;
